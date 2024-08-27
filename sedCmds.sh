d9d328e5897bdb89aa908c0744efbc126dc5e448
cole-koryto <colekoryto17@gmail.com>
2024-08-27 13:49:31 -0400

    Removing unneeded files

--- sedCmds.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-time sed -n '1~4s/^@/>/p;2~4p' input10MB.fastq > sedFASTA10MB.fasta
-time sed -n '1~4s/^@/>/p;2~4p' input100MB.fastq > sedFASTA100MB.fasta
-time sed -n '1~4s/^@/>/p;2~4p' input1GB.fastq > sedFASTA1GB.fasta
-time sed -n '1~4s/^@/>/p;2~4p' input8GB.fastq > sedFASTA8GB.fasta
